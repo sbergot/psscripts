2790be2f7b3592f53e03b0b18c71ec833226c827
Simon Bergot <sbergot@talentsoft.com>
2017-08-19 14:01:26 +0200

    initial commit

---
+++ Deduplicate.ps1
@@ -0,0 +1,113 @@
+﻿class FileHash
+{
+    [string]$directory
+    [string]$filename
+    [string]$hash
+}
+
+Function New-FileHash([string]$path) {
+    $hash = (Get-FileHash $path).Hash
+    $directory = Split-Path $path -Resolve
+    $filename = Split-Path $path -Leaf -Resolve
+    $res = New-Object FileHash
+    $res.directory = $directory
+    $res.filename = $filename
+    $res.hash = $hash
+    return $res
+}
+
+$HASH_CACHE = @{}
+$HASH_CACHE_PATH = Join-Path $HOME '.hash_chache.json'
+
+Function Save-Cache {
+    $HASH_CACHE | ConvertTo-Json > $HASH_CACHE_PATH
+}
+
+Function Load-Cache {
+    $HASH_CACHE = Get-Content $HASH_CACHE_PATH | ConvertFrom-Json
+}
+
+Function Get-Cache {
+    $HASH_CACHE
+}
+
+Function Clean-Cache {
+    $keys = $HASH_CACHE.Keys
+    $keys | % {
+        if (!(Test-Path $_)) {
+            $HASH_CACHE.Remove($_)
+        }
+    }
+}
+
+Function Build-HashDirectory([string]$path) {
+    $allfiles = Get-ChildItem $path -Recurse -File
+    $count = ($allfiles | measure).Count
+    $i = 0
+    $allfiles | % {
+        $filepath = $_.FullName
+        if ($HASH_CACHE[$filepath]) {
+            $hash = $HASH_CACHE[$filepath]
+        } else {
+            $hash = New-FileHash $filepath
+            $HASH_CACHE[$filepath] = $hash
+        }
+        $i++
+        Write-Progress -Id 2 -Activity "Computing hashes" -Status "Processed $filepath" -PercentComplete (($i * 100) / $count)
+        $hash
+    }
+}
+
+Function Save-HashDirectory([string]$target) {
+    $dirpath = (Resolve-Path $target).Path
+    $dirname = Split-Path $dirpath -Leaf
+    $path = Split-Path $dirpath -Parent
+    Build-HashDirectory $dirpath | ConvertTo-Json > (Join-Path $path "$dirname.json")
+}
+
+Function Save-HashDirectories([string]$path) {
+    $dirs = Get-ChildItem $path -Directory
+    $i = 0
+    $count = ($dirs | measure).Count
+    $dirs | % {
+        $dirpath = $_.FullName
+        $dirname = Split-Path $dirpath -Leaf
+        Write-Progress -Id 1 -Activity "Computing hashes for folders" -Status "Processing $dirname" -PercentComplete (($i * 100) / $count)
+        Build-HashDirectory $dirpath | ConvertTo-Json > (Join-Path $path "$dirname.json")
+        $i++
+    }
+}
+
+Function Load-HashDirectory([string]$path) {
+    $object = Get-Content $path | ConvertFrom-Json
+    $object | % { [FileHash]$_ }
+}
+
+Function Test-Included([string]$path1, [string]$path2) {
+    $dir1 = Load-HashDirectory $path1
+    $dir2 = Load-HashDirectory $path2
+    $group = @{}
+    $dir1 | % { $group[$_.Hash] = $true }
+    $dir2 | ? { !$group[$_.Hash] } | % {
+        Write-Warning "file not included: $($_.directory)\$($_.filename)"
+    }
+}
+
+Function GroupBy-Hash([string] $path) {
+    $hashes = Load-HashDirectory $path
+    $group = @{}
+    $hashes | % {
+        if (!$group[$_.hash]) { 
+            $group[$_.hash] = @{}
+        }
+        $group[$_.hash][$_.directory] = $true
+    }
+    return $group
+}
+
+Function Get-DuplicatedDirectories ([string]$path) {
+    $d = GroupBy-Hash $path
+    $dirs = @{}
+    $d.Keys | ? { $d[$_].Keys.Count -gt 1 } | % { $dirs[$d[$_].Keys -join "`n"] = $ture }
+    $dirs.Keys | % { '----------------'; $_ }
+}
---
+++ Flatten.ps1
@@ -0,0 +1,83 @@
+﻿Function Get-NestedFolder {
+    Param([string]$path)
+    Get-ChildItem -Path $path -Directory -Recurse | % { $_.FullName }
+}
+
+Function Get-FoldersWithFiles {
+    Param([string]$path)
+    Get-NestedFolder | % {
+        $f = Get-ChildItem -Path $_ -File
+        if (($f | measure).Count -gt 0) { $_ }
+    }
+}
+
+
+Function Get-DirectoryDate {
+    Param(
+        [string]$path,
+        [int]$days=5
+    )
+    Write-Verbose "Checking date for: $path"
+    $files = Get-ChildItem $path -File | Sort-Object { $_.LastWriteTime }
+    if (($files | measure).count -lt 2) {
+        Write-Debug "directory: $path"
+        Write-Debug "Not enough files"
+        return
+    }
+    $firstDate = $files[0].LastWriteTime;
+    $date = $firstDate
+    :loop
+    ForEach ($f in  $files) {
+        $newDate = $f.LastWriteTime
+        $span = New-TimeSpan -Start $date -End $newDate
+        if ($span -gt (New-TimeSpan -Days $days)) {
+            Write-Debug "directory: $path"
+            Write-Debug "span too big: $($span.Days)"
+            Write-Debug $date
+            Write-Debug $newDate
+            $firstDate = $null
+            break loop
+        }
+        $date = $newDate
+    }
+    return $firstDate
+}
+
+Function Get-FormattedDate([DateTime] $date) {
+    Get-Date -Date $date -Format 'yyyy-MM'
+}
+
+Function ConvertTo-FlatFolderName {
+    Param(
+        [string] $baseFolder,
+        [string] $path
+    )
+    $relPath = $path -replace [regex]::Escape($baseFolder),''
+    return $relPath.Trim('\') -split '\\' -join ' - '
+}
+
+Function Get-FormattedFolderName {
+    Param(
+        [string] $baseFolder,
+        [string] $path
+    )
+    $date = Get-DirectoryDate $path
+    $formattedDate = Get-FormattedDate $date
+    $flatName = ConvertTo-FlatFolderName $baseFolder $path
+    return "$formattedDate $flatName"
+}
+
+Function Move-ToFormattedDirs {
+    Param(
+        [string] $path
+    )
+    $alldirs = Get-NestedFolder $path
+    $dirs = $alldirs | ? { Get-DirectoryDate $_ }
+    $dirs | % {
+        $formatted = Get-FormattedFolderName $path $_
+        $dest = "$path\$formatted"
+        Write-Verbose "src:  $_"
+        Write-Verbose "dest: $dest"
+        Move-Item $_ $dest -WhatIf
+    }
+}
